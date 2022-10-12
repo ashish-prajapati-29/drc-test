2e2b98df730d7422655227015163666d011535b1
Ashish.prajapati <ashish.prajapati@thesunflowerlab.com>
2022-10-13 00:08:28 +0530

    DRC-Test: "Add additional files"

---
+++ src/main/java/com/drc/test/DrcSystemTestApplication.java
@@ -0,0 +1,13 @@
+package com.drc.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrcSystemTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrcSystemTestApplication.class, args);
+    }
+
+}
